2685cf51a072aafd8430b3a545ea580f2b3dc901
Jrc <jrc950114@gmail.com>
2025-10-15 14:08:31 +0800

    fix: :rotating_light: update eslint

--- src/assets/js/config.ts
+++ src/assets/js/config.ts
@@ -1,24 +1,20 @@
-
-import redstone from '../img/redstone.png'
+import redstone from "../img/redstone.png";
 // import copper from '../img/copper.png'
-import gold from '../img/gold.png'
-import emerald from '../img/emerald.png'
-import diamond from '../img/diamond.png'
-import lazuli from "../img/lazuli.png"
-import amethyst from "../img/amethyst.png"
+import gold from "../img/gold.png";
+import emerald from "../img/emerald.png";
+import diamond from "../img/diamond.png";
+import lazuli from "../img/lazuli.png";
+import amethyst from "../img/amethyst.png";
 // import quartz from "../img/quartz.png"
-import NetherStar from "../img/nether_star.png"
+import NetherStar from "../img/nether_star.png";
 
+import TNT from "../img/TNT.png";
 
-import TNT from "../img/TNT.png"
+const Ores = [redstone, gold, emerald, diamond, lazuli, amethyst];
 
-const Ores = [redstone, gold, emerald, diamond, lazuli, amethyst]
+const Wonders = [TNT, NetherStar];
+const Size = 8;
 
-const Wonders = [
-  TNT, NetherStar
-]
-const Size = 8
+const Variety = Ores.length;
 
-const Variety = Ores.length
-
-export default { Size, Ores, Variety, Wonders }
+export default { Size, Ores, Variety, Wonders };
